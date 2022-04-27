a60e307701b7d184873d0a94be9ec27a230f715d
Diego Sousa <diegomsousaa@gmail.com>
2022-04-27 12:23:32 -0400

    Update AxiosBackend.js

--- src/components/lib/AxiosBackend.js
+++ src/components/lib/AxiosBackend.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosBackend = axios.create({
-    baseURL: process.env.REACT_APP_AXIOS='development' ? 'http://localhost:3000/api': '/api',
+    baseURL: process.env.REACT_APP_AXIOS === 'development' ? 'http://localhost:3000/api': '/api',
     timeout: 50000,
     headers:{
         authorization: `Bearer ${window.localStorage.getItem("jwtToken")}`,
